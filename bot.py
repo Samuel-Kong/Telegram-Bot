6f1919b756e02dd478c91b4ca56fffc46a1bb573
Samuel-Kong <konglexinsamuel@gmail.com>
2025-05-25 21:48:48 +0800

    Fixed issue with echoing message triggering before command

--- bot.py
+++ bot.py
@@ -40,16 +40,14 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Hello, how are you doing?")
 
-
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
 @bot.message_handler(commands=['horoscope'])
 def sign_handler(message):
     text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
     sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, day_handler)
 
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling()
